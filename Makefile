b7a4a63d6eecdd15bb512746a3766dc54d520ca6
Ennea <Ennea@users.noreply.github.com>
2024-05-18 14:55:37 +0200

    initial commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+build:
+	gcc -Wall -Wextra -std=c2x -pedantic -o ed2k ed2k.c md4.c
---
+++ ed2k.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "md4.h"
+
+int main() {
+    uint8_t i;
+    uint32_t block_bytes_read = 0;
+    uint32_t total_blocks_read = 0;
+    uint32_t total_bytes_read = 0;
+    size_t bytes_read;
+    char buf[4096];
+    unsigned char md[16] = {0};
+    MD4_CTX ctx1, ctx2;
+
+    MD4_Init(&ctx1);
+    MD4_Init(&ctx2);
+
+    // FILE *fp = fopen("toradora_test", "rb");
+    // if (fp == NULL) {
+    //     printf("Error opening file\n");
+    //     return 1;
+    // }
+    FILE *fp = stdin;
+    // read 9728000 byte chunks; that's 2375 * 4096 block reads
+    while (true) {
+        bytes_read = fread(buf, 1, 4096, fp);
+        if (ferror(fp)) {
+            printf("Error reading file\n");
+            // fclose(fp);
+            return 1;
+        }
+
+        MD4_Update(&ctx1, buf, bytes_read);
+        block_bytes_read += bytes_read;
+        total_bytes_read += bytes_read;
+        if (block_bytes_read == 9728000) {
+            block_bytes_read = 0;
+            total_blocks_read++;
+            MD4_Final(md, &ctx1);
+            MD4_Update(&ctx2, md, 16);
+            MD4_Init(&ctx1);
+        }
+
+        if (feof(fp)) {
+            break;
+        }
+    }
+    // fclose(fp);
+
+    if (total_blocks_read == 0) {
+        MD4_Final(md, &ctx1);
+    } else {
+        MD4_Final(md, &ctx1);
+        MD4_Update(&ctx2, md, 16);
+        MD4_Final(md, &ctx2);
+    }
+    printf("%d ", total_bytes_read);
+    for (i = 0; i < 16; i++) {
+        printf("%02x", md[i]);
+    }
+}
